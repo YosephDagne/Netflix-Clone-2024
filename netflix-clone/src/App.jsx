4abae0dad22df390b0cf484cf8b9fb372272c35f
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2024-12-18 23:56:35 +0300

    header and footer

--- netflix-clone/src/App.jsx
+++ netflix-clone/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState } from "react";
 import "./App.css";
-
+import Home from "./pages/Home";
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>
-    <h1>Hello</h1>
-  </>;
+  return (
+    <>
+      <Home />
+    </>
+  );
 }
 
 export default App;
---
+++ netflix-clone/src/components/Header/Header.jsx
@@ -0,0 +1,47 @@
+import "./Header.css";
+import logo from "../../assets/images/logo.png";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsIcon from "@mui/icons-material/Notifications";
+import AccountBoxIcon from "@mui/icons-material/AccountBox";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+
+const Header = () => {
+  return (
+    <div className="header_outer_container">
+      <div className="header_container">
+        <div className="header_left">
+          <ul>
+            <li>
+              <img src={logo} alt="netflix logo" width="100" />
+            </li>
+            <li>Home</li>
+            <li>TV Show</li>
+            <li>Movies</li>
+            <li>Latest</li>
+            <li>My List</li>
+            <li>Browse by Languages</li>
+          </ul>
+        </div>
+        <div className="header_right">
+          <ul>
+            <li>
+              <SearchIcon />
+            </li>
+            <li>
+              <NotificationsIcon />
+            </li>
+            <li>
+              <AccountBoxIcon />
+            </li>
+            <li>
+              <ArrowDropDownIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;

91623cfcfadb7773a765e3b6e0870b9065f0ec3c
YosephDagne <152218628+YosephDagne@users.noreply.github.com>
2024-12-20 21:46:27 +0300

    navigation

--- netflix-clone/src/pages/Home/Home.jsx
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -2,10 +2,12 @@ import Header from "../../components/Header/Header";
 import Banner from "../../components/Banner/Banner";
 import Footer from "../../components/Footer/Footer";
 import RowList from "../../components/Rows/RowList/RowList";
+import Nav from "../../components/Nav/Nav";
 
 const Home = () => {
   return (
     <>
+      <Nav />
       <Header />
       <Banner />
       <RowList />
